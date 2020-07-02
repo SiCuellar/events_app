9fa25a40d4b60afc706c962e583f17de76b5bd3a
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2020-07-02 09:51:59 -0600

    added testing for post request

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,5 +1,17 @@
 class Api::V1::EventsController < ApplicationController
+
   def create
-    
+    new_event = Event.new(event_params)
+    if new_event.save
+      render json: "Success", status: 204
+    else
+      render json: "Fail", status: 422
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :event_type)
   end
 end
--- spec/requests/api/v1/event_request_spec.rb
+++ spec/requests/api/v1/event_request_spec.rb
@@ -1,26 +1,41 @@
 require 'rails_helper'
 
 describe "Event" do
+
   it "event can be posted to database" do
     event = Event.create(name: "test button", event_type: "click")
     headers = { "Content_Type" => "application/json", "Accept" => "application/json" }
-    params
-    body = {"event" : {
-                 "name" : "test button",
-                 "event_type" : "click",
-                 "at" : "2020-06-12T00:00:01",
-                 "button_color" : "red" }
-               }
-    post "/api/v1/events"
-    # binding.pry
-    :body => body,
-    :headers => headers
+    post "/api/v1/events",
+      :params => {"event" =>
+                    {
+                  "name"=> "test button",
+                  "event_type"=> "click",
+                  "at"=>"2020-06-12T00:00:01",
+                  "button_color"=>"red"}},
+      :headers => headers
 
+    expect(request.params.keys).to include("event")
+    expect(request.params["event"].keys).to include("name")
+    expect(request.params["event"].keys).to include("event_type")
+    
+    expect(response).to be_successful
+    expect(response.status).to eq(204)
+    expect(Event.all.count).to eq(2)
+  end
+
+  it "Sad path Error when unsuccesful create" do
+    event = Event.create(name: "test button", event_type: "click")
+    headers = { "Content_Type" => "application/json", "Accept" => "application/json" }
+    post "/api/v1/events",
+      :params => {"event" =>
+                    {
+                  "name"=> "test button",
+                  "at"=>"2020-06-12T00:00:01",
+                  "button_color"=>"red"}},
+      :headers => headers
+
+
+    expect(Event.all.count).to eq(1)
+    expect(response.status).to eq(422)
   end
 end
-
-{"location": "Denver, CO",
-  "api_key": "themuffinman",
- "controller"=>"api/v1/favorites",
- "action"=>"create",
- "user"=>{"email"=>"whatever@example.com"}}
